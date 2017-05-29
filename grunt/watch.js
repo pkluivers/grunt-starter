4411af8f9aa44b8972c1e9fc7b0a359f093fba95
Pascal <pascal@smidswater.nl>
2017-05-29 11:27:50 +0200

    added task for watching scripts

--- grunt/watch.js
+++ grunt/watch.js
@@ -3,5 +3,13 @@ module.exports = {
 	  files: 'assets/dev/scss/**/*.scss',
 	  tasks: [ 'sass:dev' ],
 	  options: { livereload: true }
+	},
+	scripts: {
+		files: 'assets/dev/js/**/*.js',
+	  	tasks: [ 'uglify' ],
+	  	options: { 
+	  		livereload: true,
+	  		spawn: false
+	  	}
 	}
 }
